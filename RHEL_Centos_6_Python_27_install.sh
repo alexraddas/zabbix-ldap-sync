5fa62a0a82c810159116043d270a2ae049864d2b
Alex Raddas <araddas@salesforce.com>
2015-07-29 19:40:43 -0700

    first commit

---
+++ RHEL_Centos_6_Python_27_install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#Run Script as root or sudo
+#!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+  echo "Run Script as root or sudo" 2>&1
+  exit 1
+else
+  mount /dev/sdb1 /mnt/disk2
+yum groupinstall "Development tools"
+yum install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel openldap-devel git-core
+
+# switch to home directory
+cd ~/
+wget http://python.org/ftp/python/2.7.6/Python-2.7.6.tar.xz
+tar xf Python-2.7.6.tar.xz
+
+cd Python-2.7.6
+
+# configure and install python2.7
+./configure --prefix=/usr/local --enable-unicode=ucs4 --enable-shared LDFLAGS="-Wl,-rpath /usr/local/lib"
+make && make altinstall
+
+# install pip
+wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py
+python2.7 ez_setup.py
+easy_install-2.7 pip
+
+pip2.7 install docopt
+pip2.7 install zabbix_api
+pip2.7 install python-ldap
+
+fi
